ef57842e940ff336a3bb38bdc0e514a95fd96074
Jamila <jamnjeri@gmail.com>
2022-11-25 17:11:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+//setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+//overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'initial'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Jamila';
+}
+//changeLeastFavoriteCustomer();
+
+var res = str.toUpperCase();  //HELLO WORLD!
